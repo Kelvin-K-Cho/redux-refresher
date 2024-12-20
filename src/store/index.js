f496efc495b2da3e6a0b56fc8f7bc10bc3e9d94d
Kelvin <kelvinchocoder@gmail.com>
2024-12-19 16:19:45 -0800

    finish project

---
+++ src/store/index.js
@@ -0,0 +1,48 @@
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from "./counter";
+import authReducer from "./auth";
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       ...state,
+//       counter: state.counter + 1,
+//     };
+//   }
+
+//   if (action.type === "increase") {
+//     return {
+//       ...state,
+//       counter: state.counter + action.amount,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       ...state,
+//       counter: state.counter - 1,
+//     };
+//   }
+
+//   if (action.type === "toggle") {
+//     return {
+//       ...state,
+//       showCounter: !state.showCounter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// const store = createStore(counterReducer);
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer,
+  },
+});
+
+export default store;
